8a1362fa0c32beb76644efb72407267203a1580b
ZhangChangquan [Õų¤ȫ] <zhangchangquan@xiaomi.com>
2014-01-29 15:33:03 +0800

    commit map using

--- hello-select.go
+++ hello-select.go
@@ -3,7 +3,7 @@ package main
 
 import (
     "fmt"
-    "time"
+//    "time"
     )
     
     
@@ -15,11 +15,11 @@ func fibonacci(c, quit chan int){
 			x,y = y,x+y
 		case <- quit:
 			fmt.Println("quit")
-		case <- time.After(5* time.Second):
-			fmt.Println("timeout")
-		    break
-		default:
-			fmt.Println("default")
+//		case <- time.After(5* time.Second):
+//			fmt.Println("timeout")
+		    return
+//		default:
+//			fmt.Println("default")
 		}
 	}
 }
@@ -36,5 +36,25 @@ func main(){
 		quit <- 3
 	}()
 	fibonacci(c,quit)
+	m := map[string]string{"key1":"val1"}
+	fmt.Println(m)
+	m["key2"]="val2"
+	fmt.Println(m)
+	delete(m,"key1")
+	fmt.Println(m)
+	
+	m_int := map[string]int {"key1":3}
+	fmt.Println(m_int)
+	m_int["key2"]=4
+	fmt.Println(m_int)
+	delete(m_int,"key1")
+	
+	
+	fmt.Println(m_int)
+	m_float := map[int]float32 {3:2.5}
+	m_float[7] = 2.0
+	fmt.Println(m_float)
+	
+	
 	
 }
